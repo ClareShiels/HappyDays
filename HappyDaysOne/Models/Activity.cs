75bfcd6fefce9b409344bd6022200bc59470a206
Clare Shiels <c_cashin@hotmail.com>
2016-10-16 17:22:34 +0100

    t

--- HappyDaysOne/Models/Activity.cs
+++ HappyDaysOne/Models/Activity.cs
@@ -19,20 +19,27 @@ namespace HappyDaysOne.Models
     public class Activity
     {
         public int ID { get; set; }
+
         [Required]
         [Display(Name = "Activity")]
         public string NameOfActivity { get; set; }
+
         public AgeGroup AgeGroup { get; set; }
+
         public ActivityType ActivityType { get; set; }
+
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         [Display(Name = "Date of First Class in Course")]
         public DateTime ActivityCourseStartDate { get; set; }
+
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         [Display(Name = "Date Of Last Class in Course")]
         public DateTime ActivityCourseEndDate { get; set; }
+
         public DayOfWeek Day { get; set; }
+
         [DataType(DataType.Time)]
         [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
         public DateTime ClassTime { get; set; }
